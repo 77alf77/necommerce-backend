6e7a28fe0b0ed20720b7c3b61b7d2e780e3ff5e3
77alf77 <pismas77@gmail.com>
2024-08-02 13:44:11 +0200

    Update Spring Boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.5.RELEASE"
+    id("org.springframework.boot") version "2.5.0"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("org.jetbrains.kotlin.plugin.jpa") version "1.4.20-RC"
     kotlin("jvm") version "1.4.10"
